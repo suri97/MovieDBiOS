83a31a5cc5c16a9aa4a7476f58fbfebc6e023c42
Suruchi Chopra <suransh2008@gmail.com>
2018-01-02 23:38:54 +0530

    Tab Bar added

---
+++ MDB/Controller/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  MDB
+//
+//  Created by Suruchi Chopra on 02/01/18.
+//  Copyright © 2018 Suransh Chopra. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+
+}
